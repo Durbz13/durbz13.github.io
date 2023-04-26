b2d36afa4bdab10a7614059a7dcc3ea747acbe5e
Durbz13 <129304137+Durbz13@users.noreply.github.com>
2023-04-26 07:05:15 +0100

    Add files via upload

---
+++ testng-folder/scripts/main.js
@@ -0,0 +1,26 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/311160.png") {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  } else {
+    myImage.setAttribute("src", "images/311160.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+myButton.onclick = () => {
+  setUserName();
+};
